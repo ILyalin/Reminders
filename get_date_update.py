48ac783cc3294d1ed4ff91f091f56fc5f9bf2a35
ILyalin <Lyalin05@inbox.ru>
2022-06-18 15:11:21 +0300

    2 test branches

--- get_date_update.py
+++ get_date_update.py
@@ -1,12 +1,12 @@
-from datetime import datetime
-
-from loader import scheduler
-
-
-def get_actual_date():
-    date_and_time_now = datetime.now()
-    print(date_and_time_now)
-
-
-def schedule_jobs():
-    scheduler.add_job(get_actual_date(), 'interval', minutes=1)
+# from datetime import datetime
+#
+# from loader import scheduler
+#
+#
+# def get_actual_date():
+#     date_and_time_now = datetime.now()
+#     print(date_and_time_now)
+#
+#
+# def schedule_jobs():
+#     scheduler.add_job(get_actual_date(), 'interval', minutes=1)
